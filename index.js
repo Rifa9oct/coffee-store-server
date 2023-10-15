0b85747982f4f3c5db217e93d8c3f4ef72a8f9b7
Rifa9oct <kohinuraktherr@gmail.com>
2023-10-16 00:07:09 +0600

    post coffee

--- index.js
+++ index.js
@@ -14,35 +14,50 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const coffeesCollection = client.db('coffeesDB').collection('coffees');
+
+        app.get("/coffees", async(req, res) => {
+            const cursor = coffeesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post("/coffees", async(req, res) => {
+            const coffees = req.body;
+            console.log("add coffee", coffees);
+            const result = await coffeesCollection.insertOne(coffees);
+            res.send(result);
+        })
 
 
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-app.get ('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send("COFFEE MAKING SERVER IS RUNNING...")
 })
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Coffee server is running on port: ${port}`)
 })
 
